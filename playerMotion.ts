7a4543a065ee2a86bcec30643b7663f56c8fb1e2
Lapis <45414333+Lapis256@users.noreply.github.com>
2023-02-10 13:27:59 +0900

    Update playerMotion.ts

--- playerMotion.ts
+++ playerMotion.ts
@@ -8,10 +8,9 @@
 import { Player, Vector3 } from "@minecraft/server";
 
 export function playerClearVelocity(player: Player) {
-    const { x, y } = player.getRotation();
-    player.teleport(player.location, player.dimension, x, y, false);
+    player.applyKnockback(0, 0, 0, 0);
 }
 
-export function playerApplyImpulse(player: Player, { x, y, z }: Vector3) {
+export function playerSetVelocity(player: Player, { x, y, z }: Vector3) {
     player.applyKnockback(x, z, Math.sqrt(x ** 2 + z ** 2) * 2.5, y);
 }
